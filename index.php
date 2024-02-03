b2fc0f5c2fd82d90b85f7faa804c0680b19e5b2d
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2024-02-03 16:46:39 +0100

    functions practice

--- index.php
+++ index.php
@@ -8,34 +8,52 @@
 </head>
 
 <body>
-    <form action="index.php" method="post">
+    <!-- <form action="index.php" method="post">
         <input type="checkbox" name="pizza" value="Pizza">Pizza<br>
         <input type="checkbox" name="hamburger" value="Hamburger">Hamburger<br>
         <input type="checkbox" name="hotdog" value="Hotdog">Hot Dog<br>
         <input type="submit" name="submit" value="Submit">
-    </form>
+    </form> -->
 </body>
 
 </html>
 
 
 <?php
-if (isset($_POST["submit"])) {
 
-    if (isset($_POST["pizza"])) {
-        echo "You selected Pizza<br>";
-    }
-    switch ($credit_card) {
-        case "Visa":
-            echo "You selected Visa";
-            break;
-        case "MasterCard":
-            echo "You selected MasterCard";
-            break;
-        case "American Express":
-            echo "You selected American Express";
-            break;
-        default:
-            echo "Please select a credit card";
-    }
+function hypotenuse($a, $b)
+{
+    $c = sqrt($a ** 2 + $b ** 2);
+    return $c;
 }
+echo hypotenuse(3, 4);
+
+function happy_birthday($first_name, $age)
+{
+    echo "Happy Birthday to you! <br>";
+    echo "Happy Birthday to you! <br>";
+    echo "Happy Birthday dear {$first_name}! <br>";
+    echo "You are {$age} years old <br>";
+}
+
+happy_birthday("Janka", 29);
+
+// if (isset($_POST["submit"])) {
+
+//     if (isset($_POST["pizza"])) {
+//         echo "You selected Pizza<br>";
+//     }
+//     switch ($credit_card) {
+//         case "Visa":
+//             echo "You selected Visa";
+//             break;
+//         case "MasterCard":
+//             echo "You selected MasterCard";
+//             break;
+//         case "American Express":
+//             echo "You selected American Express";
+//             break;
+//         default:
+//             echo "Please select a credit card";
+//     }
+// }
