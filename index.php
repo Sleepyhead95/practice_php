24bdb1b56e79df1a42bc1eea47bbedeedd6cf27e
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2024-02-04 17:34:19 +0100

    include function practice - components passing

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+include("header.html");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,83 +13,81 @@
 </head>
 
 <body>
-    <form action="index.php" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" name="username"><br>
-        <label for="age">Age:</label><br>
-        <input type="text" name="age"><br>
-        <label for="email">E-mail:</label><br>
-        <input type="text" name="email"><br><br>
-        <input type="submit" name="login" value="Log in"><br>
-    </form>
+    This is the Home Page of my PHP website.
+
 </body>
 
 </html>
 
+<?php
+
+include("footer.html");
+?>
 
 <?php
-// SANITIZATION
-if (isset($_POST["login"])) {
-    $username = filter_input(
-        INPUT_POST,
-        "username",
-        FILTER_SANITIZE_SPECIAL_CHARS
-    );
-    $age = filter_input(
-        INPUT_POST,
-        "age",
-        FILTER_SANITIZE_NUMBER_INT
-    );
-    $email = filter_input(
-        INPUT_POST,
-        "email",
-        FILTER_SANITIZE_EMAIL
-    );
-}
-//VALIDATION
-if (isset($_POST["login"])) {
-    $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
-    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-    if (empty($age)) {
-        echo "Age is required";
-    } else {
-        echo "Age is valid";
-    }
-}
+
+// // SANITIZATION
+// if (isset($_POST["login"])) {
+// $username = filter_input(
+// INPUT_POST,
+// "username",
+// FILTER_SANITIZE_SPECIAL_CHARS
+// );
+// $age = filter_input(
+// INPUT_POST,
+// "age",
+// FILTER_SANITIZE_NUMBER_INT
+// );
+// $email = filter_input(
+// INPUT_POST,
+// "email",
+// FILTER_SANITIZE_EMAIL
+// );
+// }
+// //VALIDATION
+// if (isset($_POST["login"])) {
+// $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
+// $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+// if (empty($age)) {
+// echo "Age is required";
+// } else {
+// echo "Age is valid";
+// }
+// }
 
 // function hypotenuse($a, $b)
 // {
-//     $c = sqrt($a ** 2 + $b ** 2);
-//     return $c;
+// $c = sqrt($a ** 2 + $b ** 2);
+// return $c;
 // }
 // echo hypotenuse(3, 4);
 
 // function happy_birthday($first_name, $age)
 // {
-//     echo "Happy Birthday to you! <br>";
-//     echo "Happy Birthday to you! <br>";
-//     echo "Happy Birthday dear {$first_name}! <br>";
-//     echo "You are {$age} years old <br>";
+// echo "Happy Birthday to you! <br>";
+// echo "Happy Birthday to you! <br>";
+// echo "Happy Birthday dear {$first_name}! <br>";
+// echo "You are {$age} years old <br>";
 // }
 
 // happy_birthday("Janka", 29);
 
 // if (isset($_POST["submit"])) {
 
-//     if (isset($_POST["pizza"])) {
-//         echo "You selected Pizza<br>";
-//     }
-//     switch ($credit_card) {
-//         case "Visa":
-//             echo "You selected Visa";
-//             break;
-//         case "MasterCard":
-//             echo "You selected MasterCard";
-//             break;
-//         case "American Express":
-//             echo "You selected American Express";
-//             break;
-//         default:
-//             echo "Please select a credit card";
-//     }
+// if (isset($_POST["pizza"])) {
+// echo "You selected Pizza<br>";
+// }
+// switch ($credit_card) {
+// case "Visa":
+// echo "You selected Visa";
+// break;
+// case "MasterCard":
+// echo "You selected MasterCard";
+// break;
+// case "American Express":
+// echo "You selected American Express";
+// break;
+// default:
+// echo "Please select a credit card";
+// }
 // }
